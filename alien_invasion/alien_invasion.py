f813d4b374ffd5e29ca9cd4d7110fd2811b64c10
juirian <jianrui0925@gmail.com>
2023-03-16 16:53:05 +0800

    20230316

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -14,12 +14,23 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            pygame.display.flip()
+            # for event in pygame.event.get():
+            #     if event.type == pygame.QUIT:
+            #         sys.exit()
+            self._check_events()
+            # self.screen.fill(self.settings.bg_color)
+            # self.ship.blitme()
+            # pygame.display.flip()
+            self._update_screen()
+    def _check_events(self):
+        """ 响应按键和鼠标事件 """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
